3d24020baf247c0b97882fadf5b396033f128d2c
chaospredictor <kuzi81@gmail.com>
2017-06-17 20:41:50 +0300

    list.h ver0.1.0

--- list.h
+++ list.h
@@ -8,236 +8,87 @@
 #ifndef LIST_H_
 #define LIST_H_
 
-#include <memory>
-
-template <typename T>
-class List;
-
-//template <typename TNode>
+template <class T>
+class List {
 
 
-template <typename T>
-class Node
-{
-    friend class List<T>;
-    //friend class ListIterator<Node<T>>;
-    //friend class ListIterator<const Node<T>>;
-
-    Node() : next(nullptr) {}
-    Node(const T &data) : data(data), next(nullptr) {}
-    Node<T> *next;
-    T data;
-public:
-    typedef T value_type;
-};
-
-template <typename T>
-class List
-{
-    typedef Node<T> node;
-
-    std::size_t size;
-    node *head = nullptr;
-    node *tail = nullptr;
-    node *first = nullptr;
-
-
-
-    //std::unique_ptr<node> head;
-    //std::unique_ptr<node> tail;
-    //std::unique_ptr<node> first;
-
-
-
-
-
-    void init()
-    {
-        size = 0;
-        head.reset(new node);
-        tail.reset(new node);
-        head->next = tail.get();
-    }
+	typedef struct node_t {
+		T* data;
+		struct node_t* next;
+	} *Node;
 
 public:
 
-    class Iterator {
-        //friend class List<typename TNode::value_type>;
-        node* p;
-    public:
-        Iterator(node* p) : p(p) {}
-        Iterator(const Iterator& other) : p(other.p) {}
-        Iterator operator=(Iterator other) { std::swap(p, other.p); return *this; }
-        Iterator& operator++();
-        Iterator& operator++(int) { p = p->next; }
-        Iterator operator--() { p = p->next; }
-        Iterator& operator--(int) { p = p->next; }
+	class Iterator {
+	    Node ptr;
 
-        bool operator==(const Iterator& other) { return p == other.p; }
-        bool operator!=(const Iterator& other) { return p != other.p; }
-        //const int& operator*() const { return p->data; }
-        Iterator operator+(int i)
-        {
-            Iterator iter = *this;
-            while (i-- > 0 && iter.p)
-            {
-                ++iter;
-            }
-            return iter;
-        }
-	    T& operator*();
-	    const T& operator*() const { return 1; }
-
-    };
+	public:
+	    Iterator() {}
+	    bool operator!=(const Iterator& right) {
+	    	return false;
+	    }
+	    Iterator& operator++() {
+			ptr = ptr->next;
+			return *this;
+	    }
+	    Iterator& operator++(int) {
+			ptr = ptr->next;
+			return *this;
+	    }
+	    Iterator& operator--() {
+			ptr = ptr->next;
+			return *this;
+	    }
+	    Iterator& operator--(int) {
+			ptr = ptr->next;
+			return *this;
+	    }
 
 
-    typedef Iterator iterator;
-    typedef Iterator const const_iterator;
+	    T& operator*() { return *(ptr->data); }
+	    const int& operator*() const { return ptr->data; }
 
-    //typedef ListIterator<const node> const_iterator;
 
-    List() { init(); }
+	};
 
-    List(const List& other)
-    {
-        init();
-        const_iterator i = other.begin();
-        while (i != other.end())
-        {
-            add(*i);
-            i++;
-        }
+	typedef Iterator iterator;
 
-        head.reset(other.head.get());
-        tail.reset(other.tail.get());
+
+	List() {}
+
+    bool operator==(const List& right) {
+    	return true;
     }
 
-    List(List&& other)
-    {
-        init();
-        size = other.size;
-        head = other.head;
-        tail = other.tail;
-        other.first = nullptr;
-        other.size = 0;
+    bool operator!=(const List& right) {
+    	return false;
     }
 
-    List& operator=(List other)
-    {
-        swap(*this, other);
-        return *this;
-    }
 
-    List& operator=(List&& other)
-    {
-        assert(this != &other);
-        while (head->next != tail)
-        	printf("TODO something");
-            //remove(begin());
-        head = other.head;
-        tail = other.tail;
-        size = other.size;
-        first = other.first;
-        other.size = 0;
-        other.first = nullptr;
-        return *this;
-    }
 
-    virtual ~List()
-    {
-        while (head->next != tail)
-        	printf("TODO something");
-            //remove(begin());
-    }
+    void insert(T data, iterator it) {}
+	void insert(T data) {}
 
-	bool operator==(List second) {
-		return true;
+	iterator end() {
+		Iterator it;
+		return it;
 	}
-
-	bool operator!=(List second) {
-		return false;
-}
-
-    friend void swap(List& first, List& second)
-    {
-        std::swap(first.size, second.size);
-        std::swap(first.head, second.head);
-        std::swap(first.tail, second.tail);
-    }
-
-    void add(const T &value)
-    {
-        node *first = new node(value);
-        first->next = head->next;
-        head->next = first;
-        size++;
-    }
-
-    void remove(iterator& removeIter)
-    {
-        node *last = head;
-        iterator i = begin();
-
-        while (i != removeIter)
-        {
-            last = i.p;
-            ++i;
-        }
-
-        if (i != end())
-        {
-            last->next = i.p->next;
-            size--;
-            delete i.p;
-        }
-    }
-
-    const int getSize()
-    {
-        return size;
-    }
-
-    void insert(const T &value, iterator it)
-    {
-        node *first = new node(value);
-        first->next = head->next;
-        head->next = first;
-        size++;
-    }
-
-    void insert(const T &value)
-    {
-        node *first = new node(value);
-        tail->next = first;
-        size++;
-    }
-
-    iterator begin()
-    {
-        return iterator(head->next);
-    }
-
-    const_iterator begin() const
-    {
-        return const_iterator(head->next);
-    }
-
-    iterator end()
-    {
-        return iterator(tail);
-    }
-
-    const_iterator end() const
-    {
-        return const_iterator(tail);
-    }
+	iterator begin() {
+		Iterator it;
+		return it;
+	}
+	void remove(iterator& it) {}
 
 	template<class Function>
 	void sort(Function f);
 
 	template<class Function>
 	iterator find(Function f);
-};
 
+	int getSize() { return 1; }
+
+
+
+};
 
 #endif /* LIST_H_ */
